c88126d8f2c8cc72c144be98fa766cb9bf152b40
YeiiMacc <yeiimaccdev@gmail.com>
2023-04-19 14:28:50 -0500

    Class-based Express.

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 require('dotenv').config();
 
-const express = require('express')
-const app = express()
+const Server = require('./models/server');
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
 
-app.listen( process.env.PORT, () => {
-    console.log('Servidor corriendo en el puerto: ', process.env.PORT)
-});
+const server = new Server();
+
+server.listen();
---
+++ models/server.js
@@ -0,0 +1,36 @@
+
+const express = require('express')
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || 8080;
+
+        // Middleware
+        this.middlewares();
+
+
+        this.routes();
+    }
+
+    middlewares () {
+        // Public directory
+        this.app.use(express.static('public'))
+    }
+
+
+    routes() {
+        this.app.get('/api', (req, res) => {
+            res.send('Hello World desde la API')
+        })
+    }
+
+    listen () {
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en el puerto: ', this.port)
+        });
+    }
+}
+
+module.exports = Server;
