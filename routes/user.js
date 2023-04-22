e5b37e077af4ac58a1fe8ec6a4615308104a8bbe
YeiiMacc <yeiimaccdev@gmail.com>
2023-04-22 13:39:36 -0500

    Validate role against database - Create roles in the DB.

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,7 @@ const { getUsers,
     deleteUsers } = require('../controllers/user');
 const { check } = require('express-validator');
 const { validateFields } = require('../middlewares/validate-fields');
+const Role = require('../models/role');
 
 const router = Router();
 
@@ -17,7 +18,13 @@ router.post('/', [
     check('password', 'La contraseña debe tener más de 6 letras.').isLength({ min:6 }),
     check('email', 'El email es obligatorio.').not().isEmpty(),
     check('email', 'El email no es válido.').isEmail(),
-    check('role', 'No es un rol válido.').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    // check('role', 'No es un rol válido.').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('role').custom( async(role = '') => {
+        const existsRole = await Role.findOne({ role});
+        if ( ! existsRole ) {
+            throw new Error(`El rol ${ role} no está registrado en la DB`);
+        }
+    }),
     validateFields
 ], postUsers);
 
