7e63c1318cdcfc211edbe7da06cd7920997b41d9
YeiiMacc <yeiimaccdev@gmail.com>
2023-04-24 02:48:09 -0500

    User login.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,53 @@
 const { request, response } = require("express");
+const bcryptjs = require("bcryptjs");
+
+const User = require("../models/user");
 
 
-const login = (req = request, res = response) => {
-    const {email, password} = req.body;
-    res.json({
-        message: 'Login ok',
-        email,
-        password
-    });
+
+const login = async (req = request, res = response) => {
+    try {
+        const { email, password } = req.body;
+
+        // Check if the email exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                message: 'Usuario/Password no son correctos - Email'
+            });
+        }
+
+        // Check if the user is active
+        console.log(user)
+        if (!user.status) {
+            return res.status(400).json({
+                message: 'Usuario/Password no son correctos - estado inactivo'
+            });
+        }
+
+        // Check if the password matches
+        const validPassword = bcryptjs.compareSync(password, user.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                message: 'Usuario/Password no son correctos - Password.'
+            });
+        }
+
+        // Generate the JWT
+
+        res.json({
+            message: 'Login ok',
+            email,
+            password
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error de servidor.'
+        });
+    }
+
 }
 
 module.exports = {
