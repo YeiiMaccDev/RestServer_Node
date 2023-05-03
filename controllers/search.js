397b7100bb42a211d0f86bf6f4a3af5d72efb0bf
YeiiMacc <yeiimaccdev@gmail.com>
2023-05-03 18:43:04 -0500

    Search User.

--- controllers/search.js
+++ controllers/search.js
@@ -1,15 +1,63 @@
 const { request, response } = require("express");
+const { isValidObjectId } = require("mongoose");
+
+const {
+    Category,
+    Product,
+    Role,
+    User
+} = require("../models");
 
 
+const collectionsLicensed = [
+    'categories',
+    'products',
+    'roles',
+    'users',
+];
 
-const search = async(req = request, res = response) => {
-    const { collection, query } = req.params;
+const searchUser = async (query = '', res = response) => {
+    const isMongoId = isValidObjectId(query);
+
+    if (isMongoId) {
+        const user = await User.findById(query);
+        res.json({
+            results: (user) ? [user] : []
+        });
+    }
+
 
     res.json({
-        message: 'Buscar ... ',
-        collection,
-        query
+        results: []
     });
+}
+
+const search = async (req = request, res = response) => {
+    const { collection, query } = req.params;
+
+    if (!collectionsLicensed.includes(collection)) {
+        return res.status(400).json({
+            message: `Calecciones permitidas: ${collectionsLicensed}`
+        });
+    }
+
+    switch (collection) {
+        case 'categories':
+
+            break;
+        case 'products':
+
+            break;
+        case 'users':
+            searchUser(query, res);
+            break;
+        default:
+            res.status(500).json({
+                message: `Olvidé hacer esta búsqueda ${key}`
+            });
+            break;
+    }
+
 }
 
 module.exports = {
